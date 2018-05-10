eebbb394f265075587c7273d1f5529ce677fb648
gaoying <184468338@qq.com>
2018-05-10 14:26:41 +0800

    index公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,15 @@
+if (location.href.indexOf('login.html') === -1) {
+  $.ajax({
+    url: '/employee/checkRootLogin',
+    dataType: 'json',
+    success: function (info) {
+      if (info.error === 400) {
+        location.href = 'login.html';
+        return false;
+      }
+    }
+  })
+}
 $(function () {
   NProgress.configure({showSpinner: false});
   $(document).ajaxStart(function () {
@@ -5,5 +17,29 @@ $(function () {
   });
   $(document).ajaxStop(function () {
     NProgress.done();
+  });
+  
+  $('.lt_aside .category').click(function () {
+    $('.lt_aside .child').stop().slideToggle();
+  });
+  
+  $('.icon_menu').click(function () {
+    $('.lt_aside,.lt_main,.lt_topbar').toggleClass('hidemenu');
+  });
+  
+  $('.icon_logout').click(function () {
+    $('#logoutModal').modal('show');
+  });
+  
+  $('.btnLogout').click(function () {
+    $.ajax({
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function (info) {
+        if (info.success) {
+          location.href = 'login.html';
+        }
+      }
+    })
   })
 });
